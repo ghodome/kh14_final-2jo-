42598ff536ce54b1edf0869496f1a924857e41a0
ghodome <vjaewook@gmail.com>
2024-10-02 18:32:25 +0900

    generate final project

---
+++ artauction/src/main/java/com/art/ArtauctionApplication.java
@@ -0,0 +1,13 @@
+package com.art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtauctionApplication.class, args);
+	}
+
+}
