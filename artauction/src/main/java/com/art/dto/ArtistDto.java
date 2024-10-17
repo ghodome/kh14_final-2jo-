93ed1ebaad82b9f0b97023ab797bff3dd2a682c6
ghodome <vjaewook@gmail.com>
2024-10-17 17:20:30 +0900

    작가 목록 백엔드 구현

---
+++ artauction/src/main/java/com/art/dto/ArtistDto.java
@@ -0,0 +1,13 @@
+package com.art.dto;
+
+import lombok.Data;
+
+@Data
+public class ArtistDto {
+	private int artistNo;
+	private String artistName;
+	private String artistDescription;
+	private String artistHistory;
+	private String artistBirth;
+	private String artistDeath;
+}
