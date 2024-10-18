4f6a8511b285cd310ba4701f17270c643ae33cb4
ghodome <vjaewook@gmail.com>
2024-10-18 14:24:27 +0900

    작가 등록 및 swagger 설정

--- artauction/src/main/java/com/art/restcontroller/ArtistRestController.java
+++ artauction/src/main/java/com/art/restcontroller/ArtistRestController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.art.dao.ArtistDao;
 import com.art.dto.ArtistDto;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @CrossOrigin(origins={"http://localhost:3000"})
 @RequestMapping("/artist")
@@ -22,5 +27,12 @@ public class ArtistRestController {
 	@GetMapping("/")
 	public List<ArtistDto> list(){
 		return artistDao.selectList();
+	}
+	@PostMapping("/")
+	public void regist(@RequestBody ArtistDto artistDto) {
+		log.info("artistDto={}",artistDto);
+		int artistNo=artistDao.sequence();
+		artistDto.setArtistNo(artistNo);
+		artistDao.regist(artistDto);
 	}
 }
