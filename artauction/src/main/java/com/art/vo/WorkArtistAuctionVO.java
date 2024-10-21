a2f3e3d9ca3464027cc5785b6ca005dcf92dbac4
calm285 <calm285@gmail.com>
2024-10-21 14:10:09 +0900

    work 목록, 상세, 삭제

---
+++ artauction/src/main/java/com/art/vo/WorkArtistAuctionVO.java
@@ -0,0 +1,16 @@
+package com.art.vo;
+
+import lombok.Data;
+
+@Data
+public class WorkArtistAuctionVO {
+	private int workNo;
+	private int artistNo;
+	private String workTitle;
+	private String workDescription;
+	private String workMaterials;
+	private String workSize;
+	private String workCategory;
+	private String artistName;
+	
+}
