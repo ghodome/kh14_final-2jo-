b7834091845676bb052cf26347e496fffaa9d5fb
calm285 <calm285@gmail.com>
2024-10-23 13:28:15 +0900

    파일 업로드에 필요한 작업

---
+++ artauction/src/main/java/com/art/restcontroller/FileDownloadRestController.java
@@ -0,0 +1,32 @@
+package com.art.restcontroller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.art.dao.AttachmentDao;
+import com.art.service.AttachmentService;
+
+@RestController
+@RequestMapping("/attach")
+public class FileDownloadRestController {
+	
+	@Autowired
+	private AttachmentDao attachmentDao;
+	
+	@Autowired 
+	private AttachmentService attachmentService;
+	
+	@PostMapping("/")
+	public ResponseEntity<ByteArrayResource> download(
+				@RequestParam int attachmentNo) throws IOException {
+		return attachmentService.find(attachmentNo);
+	}
+	
+}
