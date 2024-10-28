78392a08455a28d9506491a4eda5a192c3a6091a
calm285 <calm285@gmail.com>
2024-10-28 13:27:15 +0900

    작품, 작가 이미지 띄우기 작업중

---
+++ artauction/src/main/java/com/art/vo/ArtistListRequestVO.java
@@ -0,0 +1,14 @@
+package com.art.vo;
+
+import com.art.advice.JsonEmptyStringToNullDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import lombok.Data;
+
+@Data
+public class ArtistListRequestVO {
+	@JsonDeserialize(using = JsonEmptyStringToNullDeserializer.class)
+	private String column, keyword;	
+	@JsonDeserialize(using = JsonEmptyStringToNullDeserializer.class)
+	private Integer beginRow, endRow;
+}
