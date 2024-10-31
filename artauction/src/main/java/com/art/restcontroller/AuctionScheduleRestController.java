6eecbfe68b3f72d8903f6099d2c2b59d7c661e1e
wooinp92 <wooinp92@gmail.com>
2024-10-31 09:50:17 +0900

    경매일정 DAO, RestController, Mapper

--- artauction/src/main/java/com/art/restcontroller/AuctionScheduleRestController.java
+++ artauction/src/main/java/com/art/restcontroller/AuctionScheduleRestController.java
@@ -1,7 +1,9 @@
 package com.art.restcontroller;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,6 +31,7 @@ import com.art.vo.AuctionScheduleListRequestVO;
 import com.art.vo.AuctionScheduleListResponseVO;
 //import com.art.vo.MemberClaimVO;
 import com.art.vo.AuctionScheduleRequestVO;
+
 
 @CrossOrigin
 @RestController
@@ -75,32 +79,26 @@ public class AuctionScheduleRestController {
 	        }    
 	}
 	
-	
 	// 목록 조회 (이미지 포함)
 	@PostMapping("/")
 	public AuctionScheduleListResponseVO list(@RequestBody AuctionScheduleListRequestVO listRequestVO) {
 		//페이징 정보 정리
 		int count = auctionScheduleDao.countWithPaging(listRequestVO);
-		boolean last = listRequestVO.getEndRow() == null || count <= listRequestVO.getEndRow(); 
+		
+		Integer beginRow = listRequestVO.getBeginRow() != null ?  listRequestVO.getBeginRow() : 1;
+		Integer endRow = listRequestVO.getEndRow() != null ?  listRequestVO.getEndRow() : count;
+		listRequestVO.setBeginRow(beginRow);
+	    listRequestVO.setEndRow(endRow);
+	    
+		boolean last = count <= endRow;
 		
 		AuctionScheduleListResponseVO listResponseVO = new AuctionScheduleListResponseVO();
 		listResponseVO.setAuctionScheduleList(auctionScheduleDao.selectListByPaging(listRequestVO));
 		listResponseVO.setCount(count);
 		listResponseVO.setLast(last);
-		
+
 		return listResponseVO;
 	}
-	
-
-//	//상세
-//	@GetMapping("/{auctionScheduleNo}")
-//	public AuctionScheduleDto detail(@PathVariable int auctionScheduleNo) {
-//		AuctionScheduleDto auctionScheduleDto = auctionScheduleDao.selectOne(auctionScheduleNo);
-//		if(auctionScheduleDto == null) {
-//			throw new TargetNotFoundException();
-//		}
-//		return auctionScheduleDto;
-//	}
 	
 	//상세 - 이미지 포함
 	@GetMapping("/{auctionScheduleNo}")
@@ -112,7 +110,7 @@ public class AuctionScheduleRestController {
 		return auctionScheduleDto;
 	}
 	
-//	//수정
+	//수정
 //	@PutMapping("/")
 //	public void update(@RequestBody AuctionScheduleDto auctionScheduleDto) {
 //		boolean result =  auctionScheduleDao.update(auctionScheduleDto);
