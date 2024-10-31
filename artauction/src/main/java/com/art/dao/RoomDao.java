eb761b1109ec7c8304aab40e7a7e7d8daec78086
shwjdgus1234 <shwjdgus12345@naver.com>
2024-10-31 09:31:59 +0900

    실시간 채팅 DB저장 되게 오류 해결

--- artauction/src/main/java/com/art/dao/RoomDao.java
+++ artauction/src/main/java/com/art/dao/RoomDao.java
@@ -12,56 +12,49 @@ import com.art.vo.RoomVO;
 
 @Repository
 public class RoomDao {
-    
-    @Autowired
-    private SqlSession sqlSession;
-    
-    public int sequence() {
-        return sqlSession.selectOne("room.sequence");
-    }
-    
-    public void insert(RoomDto roomDto) {
-        sqlSession.insert("room.insert", roomDto);
-    }
-    
-    public List<RoomDto> selectList() {
-        return sqlSession.selectList("room.list");
-    }
-    
-    public List<RoomVO> selectList(String memberId) {
-        return sqlSession.selectList("room.listByMember", memberId);
-    }
-    
-    public RoomDto selectOne(int roomNo) {
-        return sqlSession.selectOne("room.find", roomNo);
-    }
-    
-    public boolean update(RoomDto roomDto) {
-        return sqlSession.update("room.edit", roomDto) > 0;
-    }
-    
-    public boolean delete(int roomNo) {
-        return sqlSession.delete("room.delete", roomNo) > 0;
-    }
-    
-    // 채팅방 입장
-    public void enter(RoomMemberDto roomMemberDto) {
-        sqlSession.insert("roomMember.enter", roomMemberDto);
-    }
-    
-    // 채팅방 퇴장
-    public boolean leave(RoomMemberDto roomMemberDto) {
-        return sqlSession.delete("roomMember.leave", roomMemberDto) > 0;
-    }
-    
-    // 채팅방 자격 검사
-    public boolean check(RoomMemberDto roomMemberDto) {
-        int result = sqlSession.selectOne("roomMember.check", roomMemberDto);
-        return result > 0;
-    }
-    
-    // 회원 ID로 방 조회
-    public RoomDto selectByRoomName(String memberId) {
-        return sqlSession.selectOne("room.findByRoomName", memberId);
-    }
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int sequence() {
+		return sqlSession.selectOne("room.sequence");
+	}
+	public void insert(RoomDto roomDto) {
+		sqlSession.insert("room.insert", roomDto);
+	}
+	
+	public List<RoomDto> selectList() {
+		return sqlSession.selectList("room.list");
+	}
+	public List<RoomVO> selectList(String memberId) {
+		return sqlSession.selectList("room.listByMember", memberId);
+	}
+	
+	public RoomDto selectOne(int roomNo) {
+		return sqlSession.selectOne("room.find", roomNo);
+	}
+	
+	public boolean update(RoomDto roomDto) {
+		return sqlSession.update("room.edit", roomDto) > 0;
+	}
+	
+	public boolean delete(int roomNo) {
+		return sqlSession.delete("room.delete", roomNo) > 0;
+	}
+	
+	//채팅방 입장
+	public void enter(RoomMemberDto roomMemberDto) {
+		sqlSession.insert("roomMember.enter", roomMemberDto);
+	}
+	//채팅방 퇴장
+	public boolean leave(RoomMemberDto roomMemberDto) {
+		return sqlSession.delete("roomMember.leave", roomMemberDto) > 0;
+	}
+	
+	//채팅방 자격 검사
+	public boolean check(RoomMemberDto roomMemberDto) {
+		int result = sqlSession.selectOne("roomMember.check", roomMemberDto);
+		return result > 0;
+	}
+	
 }
