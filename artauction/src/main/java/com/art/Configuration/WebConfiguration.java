88bd5be6261830bd28cdaaf71fbcbd97bf5e5a31
wooinp92 <wooinp92@gmail.com>
2024-11-04 15:12:29 +0900

    경매 출품작 불러오기 오류 수정

--- artauction/src/main/java/com/art/Configuration/WebConfiguration.java
+++ artauction/src/main/java/com/art/Configuration/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.art.Configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +14,11 @@ public class WebConfiguration implements WebMvcConfigurer{
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///D:/upload/kh14fb_test/");  // 슬래시 주의
     }
-
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 }
